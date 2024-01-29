9ba17bdbfedacb585ff5a8c1a97556b0fe2f6d4d
Nikesh Maharjan <nikeshmhr@gmail.com>
2024-01-29 18:44:48 +0545

    add submodule package dep

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+import { sum } from "sub";
+
 function main() {
   console.log("this is the main functon");
+  console.log('summing 9+1 using submodule "sub"');
+  console.log(sum(9, 1));
 }
 
 main();
