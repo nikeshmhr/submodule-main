758209319474420ee655ac418a0caaae0a4d0313
Nikesh Maharjan <nikeshmhr@gmail.com>
2024-01-29 19:33:38 +0545

    update from submodule

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-import { sum } from "sub";
+import { sum, sub } from "sub";
 
 function main() {
   console.log("this is the main functon");
   console.log('summing 9+1 using submodule "sub"');
   console.log(sum(9, 1));
+
+  console.log('subtracting 9-1 using submodule "sub"');
+  console.log(sub(9, 1));
 }
 
 main();
